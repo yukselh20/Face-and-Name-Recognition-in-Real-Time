de27127ef0c79254a3759b05a3a5defe64251ef5
Hamza Yüksel <hamzayuksel.itu@gmail.com>
2021-09-27 20:58:03 +0300

    first commit

--- 3_face-name_detection.py
+++ 3_face-name_detection.py
@@ -65,7 +65,6 @@ while True:
         cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
         cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
-
     cv2.imshow('camera', img)
     k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27 or k==ord('q'):
